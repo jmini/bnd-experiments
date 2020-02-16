74c646b173a9e3ac9c608fa605c0010604232f4a
Jeremie Bresson <jeremie.bresson@unblu.com>
2020-02-16 07:15:27 +0100

    Add groovy-xml test projects

---
+++ groovy-v3-xml-test/src/main/java/example/GroovyXmlTest.java
@@ -0,0 +1,30 @@
+package example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import groovy.util.Node;
+import groovy.xml.XmlParser;
+
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+public class GroovyXmlTest {
+
+	private final Bundle bundle = FrameworkUtil.getBundle(this.getClass());
+
+	@Test
+	public void test() throws Exception {
+		assertNotNull("This test must be run inside an OSGi framework", bundle);
+
+		String text = "    <root>\n" + 
+		"        <technology>\n" + 
+		"            <name>Groovy</name>\n" + 
+		"        </technology>\n" + 
+		"    </root>";
+		Node node = new XmlParser().parseText(text);
+
+		assertEquals("root", node.name());
+	}
+}
