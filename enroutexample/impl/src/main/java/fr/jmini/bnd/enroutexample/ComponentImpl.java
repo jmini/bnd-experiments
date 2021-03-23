16ac0d84889d318ad7da133171756d6ce879f451
Jeremie Bresson <jeremie.bresson@unblu.com>
2021-03-23 21:44:41 +0100

    Add enRoute example
    mvn org.apache.maven.plugins:maven-archetype-plugin:3.0.1:generate -B -DarchetypeGroupId=org.osgi.enroute.archetype -DarchetypeArtifactId=project -DarchetypeVersion=7.0.0 -DgroupId=fr.jmini.bnd -DartifactId=enroutexample -Dversion=1.0.0-SNAPSHOT -Dpackage=fr.jmini.bnd.enroutexample

---
+++ enroutexample/impl/src/main/java/fr/jmini/bnd/enroutexample/ComponentImpl.java
@@ -0,0 +1,10 @@
+package fr.jmini.bnd.enroutexample;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class ComponentImpl {
+    
+    //TODO add an implementation
+    
+}
---
+++ enroutexample/impl/src/test/java/fr/jmini/bnd/enroutexample/UnitTest.java
@@ -0,0 +1,12 @@
+package fr.jmini.bnd.enroutexample;
+
+import org.junit.Test;
+
+public class UnitTest {
+    
+    @Test
+    public void testSomething() {
+        //TODO add an implementation
+    }
+    
+}
